6b08c5764da1ec21ebaf62900e77932ba3170682
Frans88 <bertrandfrancois88@gmail.com>
2017-07-13 20:53:43 +0200

    include material design

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -20,6 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/css/**", "/js/**")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
