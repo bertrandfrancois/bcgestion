a81ce1f38fa1b9694cd79173bff9859e3492d879
Frans88 <bertrandfrancois88@gmail.com>
2017-07-24 21:57:24 +0200

    client validation

--- src/main/java/com/beans/Address.java
+++ src/main/java/com/beans/Address.java
@@ -3,10 +3,12 @@ package com.beans;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
 
 @Embeddable
 @NoArgsConstructor
@@ -15,14 +17,17 @@ import javax.persistence.Embeddable;
 public class Address {
 
     @NotEmpty
+    @Size(max=50)
     @Column(name = "STREET")
     private String street;
 
     @NotEmpty
+    @Size(max=5)
     @Column(name = "POST_CODE")
     private String postCode;
 
     @NotEmpty
+    @Size(max=30)
     @Column(name = "CITY")
     private String city;
 
--- src/main/java/com/beans/Client.java
+++ src/main/java/com/beans/Client.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity(name = "CLIENTS")
@@ -22,10 +25,12 @@ public class Client {
     private Long id;
 
     @NotEmpty
+    @Size(max=30)
     @Column(name = "LAST_NAME")
     private String lastName;
 
     @NotEmpty
+    @Size(max=30)
     @Column(name = "FIRST_NAME")
     private String firstName;
 
@@ -34,14 +39,17 @@ public class Client {
     private Address address;
 
     @NotEmpty
+    @Size(max=30)
     @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
 
     @NotEmpty
     @Email
+    @Size(max=30)
     @Column(name = "MAIL")
     private String mail;
 
+    @Size(max=30)
     @Column(name = "TAX_NUMBER")
     private String taxNumber;
 
